24f769194140c0a57e795b5bd3d79aba34b86974
cyrill <siril.taka@gmail.com>
2016-12-11 22:24:27 +0900

    Use <<

--- example.rb
+++ example.rb
@@ -2,9 +2,9 @@ require 'redis'
 require 'securerandom'
 
 redis = []
-redis[0] = Redis.new(port: 16379)
-redis[1] = Redis.new(port: 26379)
-redis[2] = Redis.new(port: 36379)
+redis[0] << Redis.new(port: 16379)
+redis[1] << Redis.new(port: 26379)
+redis[2] << Redis.new(port: 36379)
 
 100.times do |i|
   redis[0].setex "foo_#{i}", 1000+i, SecureRandom.base64(1024*100)
